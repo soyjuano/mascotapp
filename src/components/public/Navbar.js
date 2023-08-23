1cbb2d623e69a79c30b960b942b5cf5ea8efa5e3
soyjuano <juanxdnon@gmail.com>
2023-08-23 18:11:52 -0300

    modofocacion navbar links (agregado css )

--- src/components/public/Navbar.js
+++ src/components/public/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
@@ -9,16 +9,18 @@ const Navbar = () => {
       <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
        
         <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+
           {/* <li><a href="/" className="nav-link px-2 text-secondary">Home</a></li>
           <li><a href="#" className="nav-link px-2 text-white">Features</a></li>
           <li><a href="#" className="nav-link px-2 text-white">Pricing</a></li>
           <li><a href="#" className="nav-link px-2 text-white">FAQs</a></li>
           <li><a href="UserForm" className="nav-link px-2 text-white">Contacto</a></li> */}
-          <Link to="/" className="nav-link px-2 text-secondary">Home </Link>
-          <Link to="/" className="nav-link px-2 text-secondary">Features </Link>
-          <Link to="/" className="nav-link px-2 text-secondary">Pricing </Link>
-          <Link to="/" className="nav-link px-2 text-secondary">FAQs </Link>
-          <Link to="/UserForm" className="nav-link px-2 text-secondary">Contacto </Link>
+
+          <NavLink exact to="/" className="nav-link px-2 text-secondary" activeClassName="active">Home </NavLink>          
+          <NavLink exact to="/Features" className="nav-link px-2 text-secondary" activeClassName="active">Features </NavLink>
+          <NavLink exact to="/Pricing" className="nav-link px-2 text-secondary" activeClassName="active">Pricing </NavLink>
+          <NavLink exact to="/FAQs" className="nav-link px-2 text-secondary" activeClassName="active">FAQs </NavLink>
+          <NavLink exact to="/UserForm" className="nav-link px-2 text-secondary" activeClassName="active">Contacto </NavLink>
 
         </ul>
 
