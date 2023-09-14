76c57e7969e7febca76d132deddc33fad329078a
soyjuano <juanxdnon@gmail.com>
2023-09-14 17:37:40 -0300

    imagen en navbar , frase de MAIN

--- src/components/public/Main.js
+++ src/components/public/Main.js
@@ -17,7 +17,7 @@ export default class Main extends Component {
         <section className="py-5 text-center container">
           <div className="row py-lg-5">
             <div className="col-lg-6 col-md-8 mx-auto">
-              <h1 className="fw-light">Album example</h1>
+              <h1 className="fw-light">The world of Rick and Morty </h1>
               <p className="lead text-body-secondary">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
               <p>
                 <a href="#" className="btn btn-primary my-2">Main call to action</a>
--- src/components/public/Navbar.js
+++ src/components/public/Navbar.js
@@ -4,41 +4,39 @@ import { Link, NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
     <header className="p-3 text-bg-dark">
-    <div className="container">
-      
-      <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+      <div className="container">
 
-      
-       
-        <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
 
-          {/* <li><a href="/" className="nav-link px-2 text-secondary">Home</a></li>
-          <li><a href="#" className="nav-link px-2 text-white">Features</a></li>
-          <li><a href="#" className="nav-link px-2 text-white">Pricing</a></li>
-          <li><a href="#" className="nav-link px-2 text-white">FAQs</a></li>
-          <li><a href="UserForm" className="nav-link px-2 text-white">Contacto</a></li> */}
+          <img
+            className="App-logo"
+            height="52"
+            src="rick.png"
+            alt="rick-giratorio"
+          />
 
-          <NavLink exact to="/" className="nav-link px-2 text-secondary" activeClassName="active">Home </NavLink>          
-          <NavLink exact to="/Personajes" className="nav-link px-2 text-secondary" activeClassName="active">Personajes </NavLink>
-          <NavLink exact to="/Pricing" className="nav-link px-2 text-secondary" activeClassName="active">Pricing </NavLink>
-          <NavLink exact to="/FAQs" className="nav-link px-2 text-secondary" activeClassName="active">FAQs </NavLink>
-          {/* <NavLink exact to="/UserForm" className="nav-link px-2 text-secondary" activeClassName="active">Contacto </NavLink> */}
+          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
 
-        </ul>
+            <NavLink exact to="/" className="nav-link px-2 text-secondary" activeClassName="active">Home </NavLink>
+            <NavLink exact to="/Personajes" className="nav-link px-2 text-secondary" activeClassName="active">Personajes </NavLink>
+            <NavLink exact to="/Pricing" className="nav-link px-2 text-secondary" activeClassName="active">Pricing </NavLink>
+            <NavLink exact to="/FAQs" className="nav-link px-2 text-secondary" activeClassName="active">FAQs </NavLink>
+           
+          </ul>
 
-        <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-          <input type="search" className="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search"/>
-        </form>
+          <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+            <input type="search" className="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search" />
+          </form>
 
-        <div className="text-end">
-          <button type="button" className="btn btn-outline-light me-2">Login</button>
-          <Link to="/UserForm">
-          <button type="button" className="btn btn-warning">Sign-up</button>
-          </Link>
+          <div className="text-end">
+            <button type="button" className="btn btn-outline-light me-2">Login</button>
+            <Link to="/UserForm">
+              <button type="button" className="btn btn-warning">Sign-up</button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  </header>
+    </header>
   )
 }
 
